2bb45add69aed0040c0a25076c75b2e5d5cb0486
Mariss Tubelis <Mariss_Tubelis@mckinsey.com>
2019-11-04 15:39:39 +0100

    Fix failing voyage and improve code

--- voyages.py
+++ voyages.py
@@ -8,34 +8,28 @@ CMS_API_URL = "https://www.hurtigruten.com/rest/b2b/voyages"
 data = hs.readJsonData(CMS_API_URL)
 ctfl_env = hs.createContentfulEnvironment(config.CTFL_SPACE_ID, config.CTFL_ENV_ID, config.CTFL_MGMT_API_KEY)
 
-voyages_to_rewrite = [35896, 48549, 48953, 48152, 49082, 48826, 47889,
-    47608, 49010, 48797, 48127, 48896, 48395, 36914, 36818, 36957, 36009,
-    35950, 50739, 50705, 50671, 50619, 50595, 50526, 58353, 50458, 58330,
-    50574, 50548, 50417]
-
-# to allow more efficient multitasking with parallel workers
+# this allows running several instances of the script in parallel in order to speed the migration up
 random.shuffle(data)
 
 for voyage_from_list in data:
 
-    # there is an exception case here, which needs to be handled
-    if voyage_from_list['id'] == 34046:
+    # these voyages are not migrated because they are being rewritten for B2B
+    if voyage_from_list['id'] in [35896, 48549, 48953, 48152, 49082, 48826, 47889,
+        47608, 49010, 48797, 48127, 48896, 48395, 36914, 36818, 36957, 36009,
+        35950, 50739, 50705, 50671, 50619, 50595, 50526, 58353, 50458, 58330,
+        50574, 50548, 50417]:
+        print("%s needs to be rewritten, skipping" % voyage_from_list['id'])
         continue
 
-    needs_rewriting = voyage['id'] in voyages_to_rewrite
-    if needs_rewriting:
-        continue
-    
-    notFound = False
+    # if voyage is already added, do not re-import
     try:
         ctfl_env.entries().find(voyage_from_list['id'])
-    except contentful_management.errors.NotFoundError:
-        notFound = True
-
-    if notFound == False:
-        print("%s already added, skipping update" % voyage_from_list['id'])
+        print("%s already added, skipping" % voyage_from_list['id'])
         continue
+    except contentful_management.errors.NotFoundError:
+        pass
 
+    # load all fields for the particular voyage by calling GET voyages/{id} 
     voyage = hs.readJsonData("%s/%s" % (CMS_API_URL, voyage_from_list['id']))  
 
     hs.addEntry(
@@ -44,17 +38,17 @@ for voyage_from_list in data:
         content_type_id="voyage",
         fields=hs.fieldLocalizer('en-US',
         {
-            'name': "[TODO rewrite] " + voyage['heading'] if needs_rewriting else voyage['heading'],
+            'name': voyage['heading'],
             'id': voyage['id'],
-            'description': "TODO" if needs_rewriting else voyage['intro'],
-            'included': hs.convertToContentfulRichText("TODO" if needs_rewriting else voyage['includedInfo']),
-            'notIncluded': hs.convertToContentfulRichText("TODO" if needs_rewriting else voyage['notIncludedInfo']),
+            'description': voyage['intro'],
+            'included': hs.convertToContentfulRichText(voyage['includedInfo']),
+            'notIncluded': hs.convertToContentfulRichText(voyage['notIncludedInfo']),
             'travelSuggestionCodes': voyage['travelSuggestionCodes'],
             'duration': voyage['durationText'],
             'destinations': [hs.entryLink(voyage['destinationId'])],
             'fromPort': hs.entryLink(voyage['fromPort']),
             'toPort': hs.entryLink(voyage['toPort']),
-            'usps': None if needs_rewriting else [hs.addEntry(
+            'usps': [hs.addEntry(
                 environment=ctfl_env,
                 id="usp%d-%d" % (voyage['id'], i),
                 content_type_id="usp",
@@ -77,10 +71,10 @@ for voyage_from_list in data:
                 content_type_id="itineraryDay",
                 fields=hs.fieldLocalizer('en-US',
                 {
-                    'day': "[TODO rewrite] " + itinerary_day['day'] if needs_rewriting else itinerary_day['day'],
+                    'day': itinerary_day['day'],
                     'location': itinerary_day['location'],
-                    'name': "TODO" if needs_rewriting else itinerary_day['heading'],
-                    'description': hs.convertToContentfulRichText("TODO" if needs_rewriting else itinerary_day['body']),
+                    'name': itinerary_day['heading'],
+                    'description': hs.convertToContentfulRichText(itinerary_day['body']),
                     'images': [hs.addAsset(
                         environment=ctfl_env,
                         asset_link=media_item['highResolutionUri'],
